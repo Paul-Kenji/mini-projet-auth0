f1c5d569a2e40a2d9f0a64ec01b3877baa693a8e
Paul Kenji BOCHATON <p.bochaton@estia.local>
2025-05-30 19:44:27 -0400

    correct error hydratation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,35 @@ import { Button } from "@mui/material";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-
-
   const router = useRouter();
 
   const handleLogin = () => {
-    router.push('/api/auth/login?returnTo=/connectedPage');
+    router.push('/auth/login?returnTo=/connectedPage');
   };
   
-
   const handleSignUp = () => {
     router.push('/auth/signup?returnTo=/');
   };
 
   return (
-      <div>
-          <Button variant="contained" color="primary" component="a" onClick={handleSignUp}>
-            Inscrire
-          </Button>
+    <div>
+      <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={handleSignUp}
+      >
+        Inscrire
+      </Button>
 
-        <Button variant="outlined" color="secondary" onClick={handleLogin}>
-          Connexion
-        </Button>
+      <Button 
+        variant="outlined" 
+        color="secondary" 
+        onClick={handleLogin}
+      >
+        Connexion
+      </Button>
     </div>
+  );
 
-    //To do to remove error page when connected with deconnection button to go back in page.tsx
-    )
-    
+  // To do: remove error page when connected with disconnection button to go back in page.tsx
 }
