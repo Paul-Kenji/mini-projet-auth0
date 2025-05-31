3e30371de1cf235ef95b1a8c0ac412ab4a441a8e
Paul Kenji BOCHATON <p.bochaton@estia.local>
2025-05-30 22:25:56 -0400

    hello

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: 'loose'
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
   },
 }
 
